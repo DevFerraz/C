ce5b7d207c43a0e33ef7b050297fd21716edd0a3
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-08 19:32:06 -0300

    Primeiro codigo

---
+++ PrimeiroCodigo.c
@@ -0,0 +1,11 @@
+int main()
+{
+float valor1 = 0, valor2 = 0;
+printf("\n Digite o primeiro valor: ");
+scanf("%f", &valor1);
+printf("\n Digite o segundo valor: ");
+scanf("%f", &valor2);
+printf("\n O valor 1 eh: %.2f ", valor1);
+printf("\n O valor 2 eh: %.2f", valor2);
+printf("\n A soma dos dois valores eh de: %.2f", valor1+valor2);
+}
