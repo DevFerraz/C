6facc4dd46e760c7d32e9e718d593d5bcfd803a6
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-08 19:41:06 -0300

    exercicio 0.1

---
+++ exercicio.01.c
@@ -0,0 +1,6 @@
+int main(){
+    int x = 5;
+    int y = 10;
+printf("A variavel x foi guardada como %d e com o endereco %x", x, &x);
+printf("\n A variavel y foi guardada como %d e com o endereco %x", y, &y);
+}
