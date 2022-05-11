e489f47901af8d304d7c89cbbfa339e4e259e476
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-10 22:03:12 -0300

    Update exercicio.03.c

--- exercicio.03.c
+++ exercicio.03.c
@@ -7,13 +7,13 @@ scanf("%f", altura[1]);
 printf("Digite a altura da terceira pessoa: ");
 scanf("%f", altura[2]);
 if (altura[1] > altura[0] && altura[1] > altura[2]){
-    printf("O mais alto eh o de %.2f", altura[1]);
+    printf("O mais alto eh o de %.2f", &altura[1]);
 }
-else if (altura[2] > altura[0] && altura[2] > altura[0]){
-    printf("O mais alto eh o de %.2f", altura[0]);
+else if (altura[0] > altura[2] && altura[0] > altura[1]){
+    printf("O mais alto eh o de %.2f", &altura[0]);
 }
 else {
-    printf("O mais alto eh o de %.2f", altura[0]);
+    printf("O mais alto eh o de %.2f", &altura[0]);
 }
 return 0;
 }
